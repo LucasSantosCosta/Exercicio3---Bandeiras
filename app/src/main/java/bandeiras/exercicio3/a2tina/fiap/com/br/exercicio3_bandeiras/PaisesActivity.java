c6b10c758794727aaffc9631d9623f59652f16a7
Lucas Santos <santos.lucas00@hotmail.com>
2017-03-03 13:40:06 -0300

    Exercicio 3 Bandeiras

---
+++ app/src/main/java/bandeiras/exercicio3/a2tina/fiap/com/br/exercicio3_bandeiras/PaisesActivity.java
@@ -0,0 +1,52 @@
+package bandeiras.exercicio3.a2tina.fiap.com.br.exercicio3_bandeiras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+public class PaisesActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ImageView imgV;
+
+    String[] pais = {
+            "Argentina", "Brasil", "Franca", "Japao"
+    };
+
+    int[] fotos = {
+            R.drawable.argentina,
+            R.drawable.brasil,
+            R.drawable.franca,
+            R.drawable.japao
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paises);
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        imgV = (ImageView)findViewById(R.id.imgV);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, pais);
+        spinner.setAdapter(adapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                imgV.setImageResource(fotos[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+}
+
